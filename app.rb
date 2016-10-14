c6cabb155fbcabe4d601fd85c6eefc7e359b5ce0
tosih <sohail@runnable.com>
2016-10-14 09:38:16 -0700

    Add working server code, haml view, and assets.

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require 'haml'
+
+before do
+  # Get information from local repository
+  @gitInfo = {
+    :name => `git rev-parse --abbrev-ref HEAD`.delete!("\n"),
+    :sHA => `git rev-parse HEAD`.delete!("\n"),
+    :shortSHA => `git rev-parse --short HEAD`.delete!("\n"),
+    :lastCommitTime => `git 'log' --format="%ai" -n1 HEAD`.delete!("\n"),
+    :lastCommitMessage => `git log --format="%B" -n1 HEAD`.delete!("\n"),
+    :lastCommitAuthor => `git log --format="%aN" -n1 HEAD`.delete!("\n"),
+    :remoteOriginUrl => `git config --get-all remote.origin.url`.delete!("\n")
+  }
+end 
+
+get '/' do
+  # Serve & Interpret index.haml with github information
+  haml :index, :locals => @gitInfo
+end
